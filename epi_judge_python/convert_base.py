6af47612bbd7739d46d51a212c4c8e5c2f74f2cf
alleboudy <ahmad.alleboudy@outlook.com>
2019-07-31 13:14:33 +0200

    cnvrt base

--- epi_judge_python/convert_base.py
+++ epi_judge_python/convert_base.py
@@ -1,9 +1,28 @@
 from test_framework import generic_test
-
+import string
 
 def convert_base(num_as_string, b1, b2):
-    # TODO - you fill in here.
-    return ''
+    
+    n=0
+    neg=''
+    if num_as_string[0]=='-':
+        neg = num_as_string[0]
+        num_as_string=num_as_string.replace('-','')
+    
+    for i,d in enumerate(reversed(num_as_string)):
+        n+=string.hexdigits.index(d.lower())*b1**i
+
+    result = []
+
+    while n>0:
+        d,n =string.hexdigits[n%b2],n//b2
+        result.append(d.upper())
+       
+    
+
+    if len(result)==0:
+        return neg+'0'
+    return neg+''.join(reversed(result))
 
 
 if __name__ == '__main__':
